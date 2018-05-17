fca8e693920541b9cef8c403537a70a1183e426d
sterlingk <sterlingkoch@sbcglobal.net>
2018-05-17 10:57:53 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <router-view/>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import { AppHeader, AppFooter } from '@/components'
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is the header</div>
+</template>
+<script>
+export default {
+  name: 'AppHeader',
+  data () {
+    return { }
+  }
+}
+</script>
